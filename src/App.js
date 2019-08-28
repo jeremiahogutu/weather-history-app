40638695db16740ffb4312eee5a8ec3996e9e2ae
Jeremiah Ogutu <jogutu@pm.me>
2019-08-27 23:35:39 -0400

    added .env file and making fetch call to get data from dark sky

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {Grid} from '@material-ui/core';
 import WeatherInfo from "./components/WeatherInfo/WeatherInfo";
 
-const App = () => {
-    return (
-        <div className="App">
-            <div className='weather-container'>
-                <div className='weather-content'>
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <WeatherInfo/>
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class App extends Component {
+    getWeather = async (e) => {
+        const api_call = await fetch(`https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`);
+        const data = await api_call.json();
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className='weather-container'>
+                    <div className='weather-content'>
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <WeatherInfo/>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default App;
