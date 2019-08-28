5c2c0569ea931360c6aa0924fc2b900e7cd22116
Jeremiah Ogutu <jogutu@pm.me>
2019-08-28 02:11:10 -0400

    used and condition to display elements

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -3,13 +3,14 @@ import React from 'react';
 const WeatherInfo = props => {
     return (
         <div>
-            {props.latitude}
-            {props.longitude}
-            {props.temperature}
-            {props.timezone}
-            {props.humidity}
-            {props.hour_summary}
-            {props.icon}
+            {props.latitude && props.longitude && <h2>Current Weather</h2>}
+            {props.latitude && <p>Latitude: {props.latitude}</p>}
+            {props.longitude && <p>Longitude: {props.longitude}</p>}
+            {props.temperature && <p>Temperature: {props.temperature}</p>}
+            {props.timezone && <p>Timezone: {props.timezone}</p>}
+            {props.humidity && <p>Humidity: {props.humidity}</p>}
+            {props.hour_summary && <p>Summary: {props.hour_summary}</p>}
+            {/*icon: {props.icon}*/}
         </div>
     );
 };
